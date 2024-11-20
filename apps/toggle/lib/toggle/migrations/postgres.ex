5629078d66ce47d1cc4e733d7e8401b6c755f8b5
Chris Bailey <me@cbailey.co.uk>
2024-11-20 09:10:13 +0000

    Run format

--- apps/toggle/lib/toggle/migrations/postgres.ex
+++ apps/toggle/lib/toggle/migrations/postgres.ex
@@ -1,6 +1,8 @@
 defmodule Toggle.Migrations.Postgres do
   @moduledoc "Migrations for bootstrapping Toggle on Postgres"
 
-  defdelegate up(opts \\ []), to: Toggle.Migrations.SQLite
-  defdelegate down(opts \\ []), to: Toggle.Migrations.SQLite
+  alias Toggle.Migrations.SQLite
+
+  defdelegate up(opts \\ []), to: SQLite
+  defdelegate down(opts \\ []), to: SQLite
 end
